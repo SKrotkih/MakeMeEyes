5d0ab20748176229d5a8d10cf6808978c87f1825
Sergey Krotkih <setgey.krotkih@gmail.com>
2018-11-11 08:04:21 +0200

    add animated objects sceneKit to the video scene

--- OpenCVproject/Scenes/Video/VideoViewController.swift
+++ OpenCVproject/Scenes/Video/VideoViewController.swift
@@ -7,13 +7,17 @@
 //
 
 import UIKit
+import SceneKit
 
 class VideoViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
 
     var videoCameraWrapper : CvVideoCameraWrapper!
-    
+    private var _sceneView: SCNView!
+    private var _scnScene: SCNScene!
+    private var _cameraNode: SCNNode!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,13 +26,59 @@ class VideoViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-
         videoCameraWrapper.startCamera()
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         videoCameraWrapper.stopCamera()
+    }
+    
+    @IBAction func addSceneButtonPressed(_ sender: Any) {
+        addScene()
+    }
+    
+    private func addScene() {
+        setupView()
+        setupScene()
+        setupCamera()
+        spawnShape()
+    }
+
+    func setupView() {
+        _sceneView = SCNView(frame: self.view.frame)
+        _sceneView.allowsCameraControl = false
+        _sceneView.showsStatistics = true
+        _sceneView.backgroundColor = UIColor.clear
+        _sceneView.isUserInteractionEnabled = true
+
+        _sceneView.showsStatistics = true
+        _sceneView.allowsCameraControl = true
+        _sceneView.autoenablesDefaultLighting = true
+        
+        imageView.addSubview(_sceneView)
+    }
+    
+    func setupScene() {
+        _scnScene = SCNScene()
+        _sceneView.scene = _scnScene
+        //_scnScene.background.contents = "GeometryFighter.scnassets/Textures/Background_Diffuse.png"
+    }
+    
+    func setupCamera() {
+        _cameraNode = SCNNode()
+        _cameraNode.camera = SCNCamera()
+        _cameraNode.position = SCNVector3(x: 0, y: 0, z: 10)
+        _scnScene.rootNode.addChildNode(_cameraNode)
+    }
+    
+    func spawnShape() {
+        var geometry:SCNGeometry
+        switch ShapeType.random() {
+        default:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        }
+        let geometryNode = SCNNode(geometry: geometry)
+        _scnScene.rootNode.addChildNode(geometryNode)
     }
 }
